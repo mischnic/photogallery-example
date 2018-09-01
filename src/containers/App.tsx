814cea394b8d82eb081d853cf50fc99f81fdd581
Hannu-Pekka Heinäjärvi <hannu-pekka.heinajarvi@futurice.com>
2018-09-01 21:01:40 +0300

    Initial commit.

---
+++ src/containers/App.tsx
@@ -0,0 +1,8 @@
+import * as React from 'react';
+import * as styles from '~/styles/containers/App.scss';
+
+export default class App extends React.Component {
+  render() {
+    return <div className={styles.wrapper}>Hello world!</div>;
+  }
+}
