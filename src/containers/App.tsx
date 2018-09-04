703a91bf81d022ba48f07d659a0951ef51a76e99
Hannu-Pekka Heinäjärvi <hannu-pekka.heinajarvi@futurice.com>
2018-09-04 12:26:36 +0300

    Move routes to separate file.

--- src/containers/App.tsx
+++ src/containers/App.tsx
@@ -1,9 +1,7 @@
 import * as React from 'react';
-import { BrowserRouter as Router, Route } from 'react-router-dom';
 import { Provider } from 'mobx-react';
 import RootStore from 'stores/RootStore';
-import Gallery from 'containers/Gallery';
-import PhotoDetails from 'containers/PhotoDetails';
+import Routes from 'routes';
 import * as styles from 'styles/containers/App.scss';
 
 const rootStore = new RootStore();
@@ -12,12 +10,9 @@ export default class App extends React.Component {
   render() {
     return (
       <Provider {...rootStore}>
-        <Router>
-          <div className={styles.wrapper}>
-            <Route exact path="/" component={Gallery} />
-            <Route exact path="/photo/:photoId" component={PhotoDetails} />
-          </div>
-        </Router>
+        <div className={styles.wrapper}>
+          <Routes />
+        </div>
       </Provider>
     );
   }
---
+++ src/routes/index.tsx
@@ -0,0 +1,18 @@
+import * as React from 'react';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import RootStore from 'stores/RootStore';
+import Gallery from 'containers/Gallery';
+import PhotoDetails from 'containers/PhotoDetails';
+
+const rootStore = new RootStore();
+
+const Routes = () => (
+  <Router>
+    <Switch>
+      <Route exact path="/" component={Gallery} />
+      <Route exact path="/photo/:photoId" component={PhotoDetails} />
+    </Switch>
+  </Router>
+);
+
+export default Routes;
