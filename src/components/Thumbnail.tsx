4a53f1930f390cd0770247a3a109eb9c287fdabf
hannupekka <hannupekka@gmail.com>
2018-10-05 21:17:04 +0300

    Update styles.

--- src/components/Thumbnail.tsx
+++ src/components/Thumbnail.tsx
@@ -2,9 +2,11 @@ import * as React from 'react';
 import * as styles from '~/styles/components/Thumbnail.scss';
 
 const Thumbnail = ({ title, url }: { title: string; url: string }) => (
-  <div className={styles.wrapper}>
-    <img className={styles.img} src={url} />
-    <p>{title}</p>
-  </div>
+  <React.Fragment>
+    <div className={styles.top}>
+      <img className={styles.img} src={url} />
+    </div>
+    <div className={styles.bottom}>{title}</div>
+  </React.Fragment>
 );
 export default Thumbnail;
