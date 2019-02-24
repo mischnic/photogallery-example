a256f74e8313a54de065df0e1335df18b486b296
Niklas Mischkulnig <mischnic@users.noreply.github.com>
2019-02-24 14:30:00 +0100

    asd

---
+++ src/index.transpiled.js
@@ -0,0 +1,10 @@
+var __importStar = (this && this.__importStar) || function (mod) {
+    if (mod && mod.__esModule) return mod;
+    var result = {};
+    if (mod != null) for (var k in mod) if (Object.hasOwnProperty.call(mod, k)) result[k] = mod[k];
+    result["default"] = mod;
+    return result;
+};
+var react_pose_1 = __importStar(require("react-pose"));
+console.log(react_pose_1.default.div);
+console.log(react_pose_1.PoseGroup);
